c7bc40becf148e228b82562d09561fbf6f5a9692
Y-Technologies <127391282+ytechnologies2019@users.noreply.github.com>
2023-12-04 13:39:16 +0630

    Update variable.tf

--- stage-management/variable.tf
+++ stage-management/variable.tf
@@ -1,11 +1,11 @@
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS_ACCESS_KEY_ID"
   type        = string
-  default     = "AKIA4HC5YEKQFTY753WU"
+  default     = "AKIA4HC5YEKQFTY753WU"  #don't worry , I deleted 
 }
 
 variable "AWS_SECRET_KEY_ID" {
   description = "AWS_SECRET_KEY_ID"
   type        = string
-  default     = "lr0hyDjpKtJ14NNqXmD9z6mPaqewUD2L4hBYSr10"
+  default     = "lr0hyDjpKtJ14NNqXmD9z6mPaqewUD2L4hBYSr10"  #don't worry , I deleted 
 }
